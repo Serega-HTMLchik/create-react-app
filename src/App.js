106cfea13260df611d14a710f5d09fc6743f2157
Serega-HTMLchik <70023791+Serega-HTMLchik@users.noreply.github.com>
2021-12-16 00:43:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react"
+import React, { Component }, {useState} from "react"
 import logo from "./logo.svg"
 import "./App.css"
-import React, {useState} from 'react';
 import UserList from "./components/UserList";
 import Button from "./components/Button";
 import "./components/Style.css";
